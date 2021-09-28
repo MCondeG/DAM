0c7265222cb9b2679222d82996624f1b9769f322
MCondeG <mrcondegonz@gmail.com>
2021-09-28 02:22:12 +0200

    28/09/21

--- CURSO2/2/PSP/Spring/Prueba/src/main/java/com/example/prueba/PruebaApplication.java
+++ CURSO2/2/PSP/Spring/Prueba/src/main/java/com/example/prueba/PruebaApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PruebaApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(PruebaApplication.class, args);
+		
 	}
-
 }
