65b8a9257c4eedd165b844d09bc6a54f68dd1120
MCondeG <mrcondegonz@gmail.com>
2021-11-29 21:45:38 +0100

    29/11/21

---
+++ CURSO2/2/PSP/Spring/ServicioWebREST/src/main/java/com/ServicioWebRestApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioWebRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioWebRestApplication.class, args);
+	}
+
+}
