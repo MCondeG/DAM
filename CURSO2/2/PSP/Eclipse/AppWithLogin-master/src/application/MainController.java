d55589fea3ffc3b39b03ae5fac9d3d6ede0f1ad1
MCondeG <mrcondegonz@gmail.com>
2022-01-31 20:35:27 +0100

    31/01/22

---
+++ CURSO2/2/PSP/Eclipse/AppWithLogin-master/src/application/MainController.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+public class MainController {
+
+    @FXML
+    private BorderPane root;
+
+    @FXML
+    private Parent login;
+
+    private Parent mainView;
+
+    @FXML
+    private LoginController loginController;
+
+    public void initialize() {
+        loginController.userProperty().addListener((obs, oldUser, newUser) -> {
+            if (newUser == null) {
+                root.setCenter(login);
+                root.getScene().getWindow().sizeToScene();
+            } else {
+                if (mainView == null) {
+                    loadMainView();
+                }
+                root.setCenter(mainView);
+                root.getScene().getWindow().sizeToScene();
+            }
+        });
+    }
+
+    private void loadMainView() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                    "DataView.fxml"));
+            mainView = loader.load();
+            DataViewController controller = loader.getController();
+            controller.userProperty().bindBidirectional(
+                    loginController.userProperty());
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+}
---
+++ CURSO2/2/PSP/Eclipse/Employee-Management-System-master/README.md
@@ -0,0 +1,8 @@
+# Employee-Management-System
+This a basic java project made using Java Swings (without drag and drop). User should login then he can add, remove, view employee or he/she can update employee details. 
+There is also an option where user can change login password using a unique string KeyID(which should be updated every time a password is changed).
+P.S. I took help from a youtube video by @Code For Interview for background images and placement of different widgets/Labels i.e. thier coordinates. 
+Databases- login and employee_details
+
+Column names for login database:   username password keyid
+Column names for employee_details:  EmployeeID Name FatherName Age DOB Address Phone EmailID Education JobPost AadharNo
