76a3446e2716002b12dc407989807b1eacfea83f
MCondeG <mrcondegonz@gmail.com>
2022-03-31 03:07:26 +0200

    30/03/22

--- CURSO2/2/PMDM/RECUPERACION/Menu/app/src/main/java/com/example/menu/MainActivity.java
+++ CURSO2/2/PMDM/RECUPERACION/Menu/app/src/main/java/com/example/menu/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.menu;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private AlertDialog dialogo;
 
 
     @Override
@@ -19,32 +24,48 @@ public class MainActivity extends AppCompatActivity {
 
         this.inicializaVariables();
 
-        setSupportActionBar(toolbar);
+        setSupportActionBar(this.toolbar);
     }
-
 
     private void inicializaVariables() {
 
         this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.dialogo = alertDialogConstructor().create();
     }
 
-
+    // Creación del menu de la toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
-        /*switch(item.getItemId()){
+        switch(item.getItemId()){
             case R.id.item1:
-                hacerMetodoItem1();
+                this.dialogo.show();
                 break;
             case R.id.item2:
-                hacerMetodoItem2();
+                Toast.makeText(this, "evento 2", Toast.LENGTH_SHORT).show();
                 break;
-        }*/
+        }
         return true;
+    }
+
+
+    // Creación del alert dialog
+    @NonNull
+    private AlertDialog.Builder alertDialogConstructor() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Añadir Posición")
+                .setPositiveButton("OK", (dialog, id)->{
+                    Toast.makeText(this, "wqfwqf", Toast.LENGTH_SHORT).show();
+                    //código de evento
+                })
+                .setNegativeButton("Cancelar", (dialog, id)->{dialog.cancel();});
+
+        return builder;
     }
 }
